d8c044e43d4955d5ab56201d965dc9c815039a87
aantoniolorenzoo <acanjim2704@g.educaand.es>
2023-11-25 09:42:27 +0100

    nuevocambio

--- HALLOWEN/js/Hallowen.js
+++ HALLOWEN/js/Hallowen.js
@@ -4,10 +4,18 @@
  * 
  */
 
-function show(spider,ghosthorse,witch,arañaMovimiento){
-    document.getElementById(spider,ghosthorse,witch,arañaMovimiento).style.visibility="visible";
+function show(element){
+    document.getElementById(element).style.visibility="visible";
     /* extrae  con (get) un elemento (pinkfire) y cambia algo en su estilo (visibiliadad) de manera que pasa de estar oculto a visible*/
 }
-function hide(spider,ghosthorse,witch){
-    document.getElementById(spider,ghosthorse,witch,arañaMovimiento,).style.visibility="hidden";
+function hide(element){
+    document.getElementById(element).style.visibility="hidden";
+}
+
+function sound(element){
+    document.getElementById(element).play();
+}
+
+function pause(element) {
+    document.getElementById(element).pause();
 }
